ef28e64bd2fb7f7a944dbd29ce13802d298f2862
Van Halbert <vhalbert@redhat.com>
2018-07-15 22:10:36 -0400

    [CLOUD-1862] fix jdv tests

--- os-datavirt/added/launch/security-ldap.sh
+++ os-datavirt/added/launch/security-ldap.sh
@@ -4,28 +4,28 @@
 
 function unset_security_ldap_env() {
     # please keep these in alphabetical order
-    unset JDV_AUTH_LDAP_ALLOW_EMPTY_PASSWORDS
-    unset JDV_AUTH_LDAP_BASE_CTX_DN
-    unset JDV_AUTH_LDAP_BASE_FILTER
-    unset JDV_AUTH_LDAP_BIND_CREDENTIAL
-    unset JDV_AUTH_LDAP_BIND_DN
-    unset JDV_AUTH_LDAP_DEFAULT_ROLE
-    unset JDV_AUTH_LDAP_DISTINGUISHED_NAME_ATTRIBUTE
-    unset JDV_AUTH_LDAP_JAAS_SECURITY_DOMAIN
-    unset JDV_AUTH_LDAP_PARSE_ROLE_NAME_FROM_DN
-    unset JDV_AUTH_LDAP_PARSE_USERNAME
-    unset JDV_AUTH_LDAP_REFERRAL_USER_ATTRIBUTE_ID_TO_CHECK
-    unset JDV_AUTH_LDAP_ROLE_ATTRIBUTE_ID
-    unset JDV_AUTH_LDAP_ROLE_ATTRIBUTE_IS_DN
-    unset JDV_AUTH_LDAP_ROLE_FILTER
-    unset JDV_AUTH_LDAP_ROLE_NAME_ATTRIBUTE_ID
-    unset JDV_AUTH_LDAP_ROLE_RECURSION
-    unset JDV_AUTH_LDAP_ROLES_CTX_DN
-    unset JDV_AUTH_LDAP_SEARCH_SCOPE
-    unset JDV_AUTH_LDAP_SEARCH_TIME_LIMIT
-    unset JDV_AUTH_LDAP_URL
-    unset JDV_AUTH_LDAP_USERNAME_BEGIN_STRING
-    unset JDV_AUTH_LDAP_USERNAME_END_STRING
+    unset AUTH_LDAP_ALLOW_EMPTY_PASSWORDS
+    unset AUTH_LDAP_BASE_CTX_DN
+    unset AUTH_LDAP_BASE_FILTER
+    unset AUTH_LDAP_BIND_CREDENTIAL
+    unset AUTH_LDAP_BIND_DN
+    unset AUTH_LDAP_DEFAULT_ROLE
+    unset AUTH_LDAP_DISTINGUISHED_NAME_ATTRIBUTE
+    unset AUTH_LDAP_JAAS_SECURITY_DOMAIN
+    unset AUTH_LDAP_PARSE_ROLE_NAME_FROM_DN
+    unset AUTH_LDAP_PARSE_USERNAME
+    unset AUTH_LDAP_REFERRAL_USER_ATTRIBUTE_ID_TO_CHECK
+    unset AUTH_LDAP_ROLE_ATTRIBUTE_ID
+    unset AUTH_LDAP_ROLE_ATTRIBUTE_IS_DN
+    unset AUTH_LDAP_ROLE_FILTER
+    unset AUTH_LDAP_ROLE_NAME_ATTRIBUTE_ID
+    unset AUTH_LDAP_ROLE_RECURSION
+    unset AUTH_LDAP_ROLES_CTX_DN
+    unset AUTH_LDAP_SEARCH_SCOPE
+    unset AUTH_LDAP_SEARCH_TIME_LIMIT
+    unset AUTH_LDAP_URL
+    unset AUTH_LDAP_USERNAME_BEGIN_STRING
+    unset AUTH_LDAP_USERNAME_END_STRING
 }
 
 function add_module() {
@@ -41,35 +41,35 @@ function add_module() {
 }
 
 function configure_ldap_security_domain() {
-    if [[ -z ${JDV_AUTH_LDAP_URL} ]]; then
-        log_info "JDV_AUTH_LDAP_URL not set. Skipping LDAP integration..."
+    if [[ -z ${AUTH_LDAP_URL} ]]; then
+        log_info "AUTH_LDAP_URL not set. Skipping LDAP integration..."
         return
     fi
-    log_info "JDV_AUTH_LDAP_URL is set to ${JDV_AUTH_LDAP_URL}. Added LdapExtended login-module"
+    log_info "AUTH_LDAP_URL is set to ${AUTH_LDAP_URL}. Added LdapExtended login-module"
     local security_domain='<login-module code="LdapExtended" flag="required">'
 
-    security_domain=$(add_module "$security_domain" "java.naming.provider.url" "${JDV_AUTH_LDAP_URL}") 
-    security_domain=$(add_module "$security_domain" "jaasSecurityDomain" "${JDV_AUTH_LDAP_JAAS_SECURITY_DOMAIN}") 
-    security_domain=$(add_module "$security_domain" "bindDN" "${JDV_AUTH_LDAP_BIND_DN}") 
-    security_domain=$(add_module "$security_domain" "bindCredential" "${JDV_AUTH_LDAP_BIND_CREDENTIAL}") 
-    security_domain=$(add_module "$security_domain" "baseCtxDN" "${JDV_AUTH_LDAP_BASE_CTX_DN}") 
-    security_domain=$(add_module "$security_domain" "baseFilter" "${JDV_AUTH_LDAP_BASE_FILTER}")
-    security_domain=$(add_module "$security_domain" "rolesCtxDN" "${JDV_AUTH_LDAP_ROLES_CTX_DN}") 
-    security_domain=$(add_module "$security_domain" "roleFilter" "${JDV_AUTH_LDAP_ROLE_FILTER}")
-    security_domain=$(add_module "$security_domain" "roleAttributeID" "${JDV_AUTH_LDAP_ROLE_ATTRIBUTE_ID}")
-    security_domain=$(add_module "$security_domain" "roleAttributeIsDN" "${JDV_AUTH_LDAP_ROLE_ATTRIBUTE_IS_DN}")
-    security_domain=$(add_module "$security_domain" "roleNameAttributeID" "${JDV_AUTH_LDAP_ROLE_NAME_ATTRIBUTE_ID}")
-    security_domain=$(add_module "$security_domain" "defaultRole" "${JDV_AUTH_LDAP_DEFAULT_ROLE}")
-    security_domain=$(add_module "$security_domain" "roleRecursion" "${JDV_AUTH_LDAP_ROLE_RECURSION}")
-    security_domain=$(add_module "$security_domain" "distinguishedNameAttribute" "${JDV_AUTH_LDAP_DISTINGUISHED_NAME_ATTRIBUTE}")
-    security_domain=$(add_module "$security_domain" "parseRoleNameFromDN" "${JDV_AUTH_LDAP_PARSE_ROLE_NAME_FROM_DN}")
-    security_domain=$(add_module "$security_domain" "parseUsername" "${JDV_AUTH_LDAP_PARSE_USERNAME}")
-    security_domain=$(add_module "$security_domain" "usernameBeginString" "${JDV_AUTH_LDAP_USERNAME_BEGIN_STRING}")
-    security_domain=$(add_module "$security_domain" "usernameEndString" "${JDV_AUTH_LDAP_USERNAME_END_STRING}")
-    security_domain=$(add_module "$security_domain" "searchTimeLimit" "${JDV_AUTH_LDAP_SEARCH_TIME_LIMIT}")
-    security_domain=$(add_module "$security_domain" "searchScope" "${JDV_AUTH_LDAP_SEARCH_SCOPE}")
-    security_domain=$(add_module "$security_domain" "allowEmptyPasswords" "${JDV_AUTH_LDAP_ALLOW_EMPTY_PASSWORDS}")
-    security_domain=$(add_module "$security_domain" "referralUserAttributeIDToCheck" "${JDV_AUTH_LDAP_REFERRAL_USER_ATTRIBUTE_ID_TO_CHECK}") 
+    security_domain=$(add_module "$security_domain" "java.naming.provider.url" "${AUTH_LDAP_URL}") 
+    security_domain=$(add_module "$security_domain" "jaasSecurityDomain" "${AUTH_LDAP_JAAS_SECURITY_DOMAIN}") 
+    security_domain=$(add_module "$security_domain" "bindDN" "${AUTH_LDAP_BIND_DN}") 
+    security_domain=$(add_module "$security_domain" "bindCredential" "${AUTH_LDAP_BIND_CREDENTIAL}") 
+    security_domain=$(add_module "$security_domain" "baseCtxDN" "${AUTH_LDAP_BASE_CTX_DN}") 
+    security_domain=$(add_module "$security_domain" "baseFilter" "${AUTH_LDAP_BASE_FILTER}")
+    security_domain=$(add_module "$security_domain" "rolesCtxDN" "${AUTH_LDAP_ROLES_CTX_DN}") 
+    security_domain=$(add_module "$security_domain" "roleFilter" "${AUTH_LDAP_ROLE_FILTER}")
+    security_domain=$(add_module "$security_domain" "roleAttributeID" "${AUTH_LDAP_ROLE_ATTRIBUTE_ID}")
+    security_domain=$(add_module "$security_domain" "roleAttributeIsDN" "${AUTH_LDAP_ROLE_ATTRIBUTE_IS_DN}")
+    security_domain=$(add_module "$security_domain" "roleNameAttributeID" "${AUTH_LDAP_ROLE_NAME_ATTRIBUTE_ID}")
+    security_domain=$(add_module "$security_domain" "defaultRole" "${AUTH_LDAP_DEFAULT_ROLE}")
+    security_domain=$(add_module "$security_domain" "roleRecursion" "${AUTH_LDAP_ROLE_RECURSION}")
+    security_domain=$(add_module "$security_domain" "distinguishedNameAttribute" "${AUTH_LDAP_DISTINGUISHED_NAME_ATTRIBUTE}")
+    security_domain=$(add_module "$security_domain" "parseRoleNameFromDN" "${AUTH_LDAP_PARSE_ROLE_NAME_FROM_DN}")
+    security_domain=$(add_module "$security_domain" "parseUsername" "${AUTH_LDAP_PARSE_USERNAME}")
+    security_domain=$(add_module "$security_domain" "usernameBeginString" "${AUTH_LDAP_USERNAME_BEGIN_STRING}")
+    security_domain=$(add_module "$security_domain" "usernameEndString" "${AUTH_LDAP_USERNAME_END_STRING}")
+    security_domain=$(add_module "$security_domain" "searchTimeLimit" "${AUTH_LDAP_SEARCH_TIME_LIMIT}")
+    security_domain=$(add_module "$security_domain" "searchScope" "${AUTH_LDAP_SEARCH_SCOPE}")
+    security_domain=$(add_module "$security_domain" "allowEmptyPasswords" "${AUTH_LDAP_ALLOW_EMPTY_PASSWORDS}")
+    security_domain=$(add_module "$security_domain" "referralUserAttributeIDToCheck" "${AUTH_LDAP_REFERRAL_USER_ATTRIBUTE_ID_TO_CHECK}") 
     
     security_domain="${security_domain}"'</login-module><!-- ##OTHER_LOGIN_MODULES## -->'
 
