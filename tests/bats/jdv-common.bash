649d8052a98a87864e1881e02f497d7bfe14927c
Van Halbert <vhalbert@redhat.com>
2018-07-23 17:04:05 -0400

    Revert "[CLOUD-1862] Need Ability to configure LDAP authentication for datavirt on Openshift (#4)"
    This reverts commit ef12dcf9c2abf06cf0f425ec5e1614eb02f7686a.

--- tests/bats/jdv-common.bash
+++
@@ -1,9 +0,0 @@
-load ./common/logging
-
-function assert_xml() {
-  local file=$1
-  local expected=$2
-  local xml=$(xmllint $file)
-
-  diff -Ew <(echo $xml | xmllint --format -) $expected
-}
--- tests/features/README.md
+++
@@ -1,3 +0,0 @@
-This directory contains CTF tests for Red Hat JBoss Middleware OpenShift container images.
-
-Please note that this directory will be rearranged in the near future.
